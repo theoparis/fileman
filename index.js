9dfd24e738b3fd59993b15eeb7f9ffdd1ac082b9
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-11-12 15:08:36 -0500

    basic file manager

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 const fs = require("fs");
 const path = require("path");
 const readline = require("readline");
+const termSize = require("./termSize");
+const chalk = require("chalk");
 
-readline.emitKeypressEvents(process.stdin);
+const State = {
+  viewingDir: 1,
+  editingFile: 2,
+};
 
 let currentDir = process.cwd();
-function listDir() {
-  let files = fs.readdirSync(currentDir);
-  let fileMap = files.map((f) => {
-    let fullPath = path.join(currentDir, f);
-    let stat = fs.statSync(fullPath);
-    return { name: f.split(/[\/\\]/g).pop(), path: fullPath, isDir: stat.isDirectory() };
-  });
-  console.log(fileMap);
-}
-listDir();
+let scroll = 0;
+let state = State.viewingDir;
 
+console.clear();
+readline.emitKeypressEvents(process.stdin);
 process.stdin.on("keypress", (ch, key) => {
-  console.log('got "keypress"', ch, key);
   if (key && key.ctrl && key.name == "c") {
     console.log("Exiting...");
     process.exit();
   }
-  console.log("\n");
-  console.log(ch);
+  if (!key.ctrl && !key.meta && !key.shift) {
+    if (state == State.viewingDir) {
+      switch (key.name) {
+        case "up":
+        case "w":
+          scroll--;
+          break;
+        case "down":
+        case "s":
+          scroll++;
+          break;
+      }
+    }
+  }
+  if (state == State.viewingDir) listDir();
 });
 
 if (process.stdin.isTTY) process.stdin.setRawMode(true);
@@ -32,3 +43,30 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+function listDir() {
+  console.clear();
+  let files = fs.readdirSync(currentDir);
+  let fileMap = files.map((f) => {
+    let fullPath = path.join(currentDir, f);
+    let stat = fs.statSync(fullPath);
+    return { name: f.split(/[\/\\]/g).pop(), path: fullPath, isDir: stat.isDirectory() };
+  });
+  fileMap = fileMap.sort((f1, f2) => {
+    f1.name.toLowerCase() > f2.name.toLowerCase() ? 1 : -1;
+  });
+  let maxScroll = fileMap.length - 1;
+  if (scroll < 0) scroll = maxScroll;
+  if (scroll > maxScroll) scroll = 0;
+  let write = [];
+  fileMap.forEach((f, ind) => {
+    let sliced = f.name.slice(0, termSize().width / 2);
+    if (f.isDir) sliced = chalk.blue(sliced);
+    if (scroll == ind) sliced = chalk.bgWhite.black(sliced);
+    write.push(`${sliced}`);
+  });
+  process.stdout.cursorTo(0, 0);
+  console.log(write.join("\n"));
+  process.stdout.cursorTo(0, termSize().height);
+}
+listDir();
---
+++ termSize.js
@@ -0,0 +1,25 @@
+const create = (columns, rows) => ({
+  columns: Number.parseInt(columns, 10),
+  width: Number.parseInt(columns, 10),
+  rows: Number.parseInt(rows, 10),
+  height: Number.parseInt(rows, 10),
+});
+
+module.exports = function termSize() {
+  // from the npm package 'term-size'
+  const { env, stdout, stderr } = process;
+
+  if (stdout && stdout.columns && stdout.rows) {
+    return create(stdout.columns, stdout.rows);
+  }
+
+  if (stderr && stderr.columns && stderr.rows) {
+    return create(stderr.columns, stderr.rows);
+  }
+
+  if (env.COLUMNS && env.LINES) {
+    return create(env.COLUMNS, env.LINES);
+  }
+
+  return create(80, 24);
+};
